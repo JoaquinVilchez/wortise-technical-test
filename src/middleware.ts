dbe3648104c0cfa58d563d962b637013747d50fc
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2025-04-24 14:03:59 -0300

    FEATURE: Middleware implementation

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { getSessionCookie } from 'better-auth/cookies';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const session = getSessionCookie(request);
+  const { pathname } = request.nextUrl;
+
+  const publicPaths = ['/login', '/signup'];
+  const protectedPaths = ['/'];
+
+  if (session && publicPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  if (!session && protectedPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/', '/login', '/signup'],
+};
