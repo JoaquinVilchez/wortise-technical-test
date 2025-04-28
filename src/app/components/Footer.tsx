a5cd58ebc44179a60398f78a845e045bfad8e174
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2025-04-28 09:56:49 -0300

    UPDATE: Footer update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,15 +2,29 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="w-full text-center py-4 text-gray-500 bg-gray-100  text-sm border-t border-gray-200 mt-10">
-      Desarrollado por{' '}
-      <Link
-        href="https://www.linkedin.com/in/joaquinvilchez"
-        className="hover:underline"
-        target="_blank"
-      >
-        Joaquín Vilchez
-      </Link>
+    <footer className="w-full text-center py-4 text-gray-500 bg-gray-100 text-sm border-t border-gray-200 mt-10">
+      <div className="flex justify-center space-x-4">
+        <span>
+          Desarrollado por{' '}
+          <Link
+            href="https://www.linkedin.com/in/joaquinvilchez"
+            className="hover:underline"
+            target="_blank"
+          >
+            Joaquín Vilchez
+          </Link>
+        </span>
+        <span>|</span>
+        <span>
+          <Link
+            href="https://github.com/JoaquinVilchez/wortise-technical-test"
+            className="hover:underline"
+            target="_blank"
+          >
+            Repositorio
+          </Link>
+        </span>
+      </div>
     </footer>
   );
 }
