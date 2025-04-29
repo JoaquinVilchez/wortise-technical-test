2c885a9703738b74ed51f45eea1e78902573a39c
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2025-04-29 13:23:46 -0300

    FIX: All HTTP images support

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.pexels.com',
-        pathname: '/photos/**',
+        hostname: '**',
       },
     ],
   },
